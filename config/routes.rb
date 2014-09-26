8a1d283696d76c1521fa5cce5304a5df6f3bb3e3
Chris Adams <cadams@pivotallabs.com>
2014-09-26 15:51:15 -0700

    Add a simple API to get latest
    Signed-off-by: Haydon Ryan <hryan@pivotallabs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BoshArtifacts::Application.routes.draw do
   root "root#show"
   resources :file_collections, only: [:index]
+  get '/api/latest_stemcell', to: 'api#latest_stemcell'
+  get '/api/latest_release', to: 'api#latest_release'
 end
